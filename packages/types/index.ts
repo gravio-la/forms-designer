00d5358ff5e8b6485db44fa80068f52435f97678
winzlieb <winzlieb@c3d2.de>
2025-09-24 00:34:57 +0200

    enhance JSON schema types and add utility functions

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,13 +1,26 @@
 import {
-  JsonSchema,
   Labelable,
   Scopable,
   UISchemaElement,
-  TesterContext,
   JsonFormsRendererRegistryEntry,
+  JsonSchema4,
+  JsonSchema7,
 } from '@jsonforms/core'
+import { JSONSchema4, JSONSchema7 } from 'json-schema'
+
+export type JsonSchema = JSONSchema7 | JSONSchema4| JsonSchema4 | JsonSchema7;
+
+export type JsonSchemaDefinition = JsonSchema | boolean
+
+export const isJsonSchema = (
+  schema: JsonSchemaDefinition | null | undefined
+): schema is JsonSchema => {
+  return schema !== null && schema !== undefined && typeof schema === 'object'
+}
 
 export type ScopableUISchemaElement = UISchemaElement & Scopable & Labelable
+
+export const isScopableUISchemaElement = (element: any): element is ScopableUISchemaElement => element.scope
 
 export type DraggableMeta = {
   name: string
@@ -32,29 +45,36 @@ export type JsonFormsEditState = {
   editMode: boolean
 }
 
-export type RankedToolTester = (uischema: UISchemaElement, schema: JsonSchema, context: TesterContext) => number
+export type TesterContext<T extends JsonSchema = JsonSchema> = {
+    rootSchema: T;
+    config: any;
+}
+export type RankedTester<T extends JsonSchema = JsonSchema> = (uischema: UISchemaElement, schema: T, context: TesterContext) => number;
 
-type ToolSettingsAbstract = {}
-
-export type ToolSettingsMixin = {
-  mapWizardToAddonData: (previousData, wizardSchema: JsonSchema | null, uiSchema: any) => any
-  mapAddonDataToWizardSchema?: (toolData: any, wizardSchema: JsonSchema) => JsonSchema
-  mapAddonDataToWizardUISchema: (toolData: any, uiSchema: any) => any
-  jsonSchemaElement: JsonSchema['properties']
+export type ToolSettingsMixin<T extends JsonSchema = JsonSchema> = {
+  mapWizardToAddonData: (previousData, wizardSchema: T | null, uiSchema: any) => any
+  mapAddonDataToWizardSchema?: (toolData: any, wizardSchema: T, rootSchema: T) => T
+  mapAddonDataToWizardUISchema: (toolData: any, uiSchema: any, rootSchema: T) => any
+  jsonSchemaElement: T['properties']
 }
 
-export type ToolSetting = ToolSettingsAbstract & {
-  mapWizardSchemaToToolData: (wizardSchema: JsonSchema | null, uiSchema: any) => any
-  mapToolDataToWizardSchema: (toolData: any, wizardSchema: JsonSchema) => JsonSchema
-  mapToolDataToWizardUischema: (toolData: any, wizardUiSchema: any) => any
-  tester: RankedToolTester
-  jsonSchema: JsonSchema
-  toolSettingsMixins: ToolSettingsMixin[]
+export type ToolSettingsJsonSchema<T extends JsonSchema = JsonSchema> = T | ((rootSchema: T) => T)
+
+export type ToolSetting<T extends JsonSchema = JsonSchema> = {
+  mapWizardSchemaToToolData: (wizardSchema: T | null, uiSchema: any) => any
+  mapToolDataToWizardSchema: (toolData: any, wizardSchema: T, rootSchema: T) => T
+  mapToolDataToWizardUischema: (toolData: any, wizardUiSchema: any, rootSchema: T) => any
+  tester: RankedTester<T>
+  jsonSchema: ToolSettingsJsonSchema<T>
+  toolSettingsMixins: ToolSettingsMixin<T>[]
 }
 
-export type PluggableToolDefinition = {
+export type ToolSettingFunction<T extends JsonSchema = JsonSchema> = (jsonSchema: T) => ToolSetting<T>
+export type ToolSettings<T extends JsonSchema = JsonSchema> = ToolSetting<T>[]
+
+export type PluggableToolDefinition<T extends JsonSchema = JsonSchema> = {
   rendererRegistry: JsonFormsRendererRegistryEntry[]
   dropRendererRegistry: JsonFormsRendererRegistryEntry[]
-  toolSettings: ToolSetting[]
+  toolSettings: ToolSettings<T>
   toolBoxElements: DraggableElement[]
 }

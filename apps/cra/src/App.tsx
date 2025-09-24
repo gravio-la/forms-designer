f4a65082f078d79e723712c4278fa490adaf10e6
winzlieb <winzlieb@c3d2.de>
2025-09-24 23:51:37 +0200

    Update index.html meta description and title to reflect Forms Designer; refactor App component to use WizardApp instead of WizardProvider and MainLayout.

--- apps/cra/src/App.tsx
+++ apps/cra/src/App.tsx
@@ -1,10 +1,7 @@
-import { locationTools } from '@formswizard/experimental-renderers'
-import { WizardProvider, MainLayout } from '@formswizard/forms-designer'
+import { WizardApp } from '@formswizard/forms-designer'
 
 export const App = () => {
   return (
-    <WizardProvider>
-      <MainLayout additionalToolSettings={locationTools.toolSettings} renderers={locationTools.rendererRegistry} />
-    </WizardProvider>
+    <WizardApp />
   )
 }

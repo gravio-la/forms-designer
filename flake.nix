805001ff14b45239e710a88e0111b59be710786b
winzlieb <winzlieb@c3d2.de>
2025-09-25 15:11:50 +0200

    Add a script to test GitHub Pages workflow using act

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
       let pkgs = nixpkgs.legacyPackages.${system}; in rec {
         packages.cra = import ./apps/cra/shell.nix {inherit pkgs;};  ## TODO  real package
         packages.next = import ./apps/web/shell.nix {inherit pkgs;};  ## TODO  real package
+        packages.test-gh-pages-workflow = pkgs.writeShellScriptBin "test-gh-pages-workflow" ''
+          echo "Testing GitHub Pages workflow with act..."
+          ${pkgs.act}/bin/act -W .github/workflows/pages.yml --job build -P ubuntu-24.04=catthehacker/ubuntu:act-latest --env GITHUB_TOKEN=fake_token
+        '';
         defaultPackage = packages.next;
 
         devShell = pkgs.mkShell {

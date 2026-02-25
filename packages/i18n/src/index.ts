e506fbf431da412575c64f4d600b99c5dbf6fa1d
winzlieb <winzlieb@c3d2.de>
2026-02-25 01:55:36 +0100

    Add i18n package with initial setup including configuration files, translation instance, and provider component for internationalization support.

---
+++ packages/i18n/src/index.ts
@@ -0,0 +1,3 @@
+export { I18nProvider, type I18nProviderProps } from './I18nProvider'
+export { addTranslations, i18nInstance, initI18n, FORMSDESIGNER_NS, DEFAULT_LANGUAGE } from './i18nInstance'
+export { useJsonFormsI18n } from './useJsonFormsI18n'
